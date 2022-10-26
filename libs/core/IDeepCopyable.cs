f5326022050d9ae0d14c09e4c05829eef63f8277
Aurelien Bidon <abidon@protonmail.com>
2022-10-27 00:00:55 +0200

    ✨ Add IMutableDeepCopyable interface

--- libs/core/IDeepCopyable.cs
+++ libs/core/IDeepCopyable.cs
@@ -4,3 +4,8 @@ public interface IDeepCopyable<out T>
 {
   public T DeepCopy();
 }
+
+public interface IMutableDeepCopyable<T, TMutable> : IDeepCopyable<T>
+{
+  public TMutable MutableDeepCopy();
+}
